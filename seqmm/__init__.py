273af281b5122ed61a0d75e8b0eb138110b40d9b
SAAS R7 User1 <r7user1@research07.saas.hku.hk>
2019-05-10 17:44:13 +0800

    update

--- seqmm/__init__.py
+++ seqmm/__init__.py
@@ -1,6 +1,7 @@
 from .pysequd import *
 from .pybayopt import *
 from .pybatdoe import *
+from .sk_seqrand import *
 
-__all__ =  ["SeqUDSklearn", "SeqUDOptimizer", "GPEISklearn", 
-        "SMACSklearn", "TPESklearn", "GridSklearn", "RandSklearn", "LHSSklearn", "SobolSklearn", "UDSklearn"]
+__all__ =  ["SeqUDSklearn", "SeqUDOptimizer", "GPEISklearn", "SMACSklearn", "TPESklearn", "SeqRandSklearn",
+        "GridSklearn", "RandSklearn", "LHSSklearn", "SobolSklearn", "UDSklearn"]
