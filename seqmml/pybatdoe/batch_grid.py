3bef5d056b66b4d1492d5b85361772c47243a351
SAAS R7 User1 <r7user1@research07.saas.hku.hk>
2020-06-30 20:20:55 +0800

    modify grid search strategy for max_runs ** (1/s) is not int

--- seqmml/pybatdoe/batch_grid.py
+++ seqmml/pybatdoe/batch_grid.py
@@ -128,9 +128,10 @@ class GridSearch(BatchBase):
                 grid_para[item] = values['Wrapper'](np.linspace(values['Range'][0], values['Range'][1], grid_number))
             if (values['Type'] == "integer"):
                 grid_para[item] = np.round(np.linspace(min(values['Mapping']), max(values['Mapping']), grid_number)).astype(int)
+            grid_number = np.ceil(clf.max_runs / grid_number).astype(int)
         # generate grid
         para_set = pd.DataFrame([item for item in product(*grid_para.values())], columns=grid_para.keys())
-        para_set = para_set.iloc[:self.max_runs, :]
+        para_set = para_set.sample(self.max_runs)
         candidate_params = [{para_set.columns[j]: para_set.iloc[i, j]
                              for j in range(para_set.shape[1])}
                             for i in range(para_set.shape[0])]
