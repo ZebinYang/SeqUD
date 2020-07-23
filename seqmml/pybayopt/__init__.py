62c96cdcf4eeede02ec921aa4631122a9cbba72d
SAAS R7 User1 <r7user1@research07.saas.hku.hk>
2020-07-23 20:28:09 +0800

    change package name

--- seqmml/pybayopt/__init__.py
+++
@@ -1,5 +0,0 @@
-from .bayopt_gpei import GPEIOPT
-from .bayopt_smac import SMACOPT
-from .bayopt_tpe import TPEOPT
-
-__all__ = ["GPEIOPT", "SMACOPT", "TPEOPT"]
--- seqmml/pysequd/__init__.py
+++
@@ -1,5 +0,0 @@
-from .sequd import SeqUD
-from .snto import SNTO
-from .seqrand import SeqRand
-
-__all__ = ["SeqUD", "SNTO", "SeqRand"]
